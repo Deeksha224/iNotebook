b4dd05a8be3d597887868b7906e4688804191346
Deeksha Agarwal <your.email@example.com>
2025-06-06 20:10:05 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((err) => {
+            console.error("Failed to connect to MongoDB", err);
+        });
+}
+module.exports = connectToMongo;
+
